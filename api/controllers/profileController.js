3aef88461663724cf62e619543d708136880f8e2
carlos <radix_10339@hotmail.com>
2019-09-17 09:17:25 -0500

    fix typo, add error status and indent

--- api/controllers/profileController.js
+++ api/controllers/profileController.js
@@ -4,45 +4,62 @@ var mongoose = require('mongoose'),
   Profile = mongoose.model('Profile');
 
 exports.list_all_profiles = function(req, res) {
-  Profile.find({}, function(err, profile) {
-    if (err)
-      res.send(err);
-    res.json(profile);
-  });
+  Profile.find({},
+    function(err, profile) {
+      if (err){
+        res.status(400).send(err);
+      }
+      res.json(profile);
+    }
+  );
 };
 
 exports.create_a_profile = function(req, res) {
-
   var new_profile = new Profile(req.body);
-  new_profile.save(function(err, profile) {
-    if (err)
-      res.send(err);
-    res.json(profile);
-  });
+  new_profile.save(
+    function(err, profile) {
+      if (err){
+        res.status(400).send(err);
+      }
+      res.json(profile);
+    }
+  );
 };
 
 exports.read_a_profile = function(req, res) {
-  Profile.findById(req.params.profileId, function(err, profile) {
-    if (err)
-      res.send(err);
-    res.json(profile);
-  });
+  Profile.findById(
+    eq.params.profileId,
+    function(err, profile) {
+      if (err){
+        res.status(400).send(err);
+      }
+      res.json(profile);
+    }
+  );
 };
 
 exports.update_a_profile = function(req, res) {
-  Profile.findOneAndUpdate({_id: req.params.profileId}, req.body, {new: true}, function(err, profile) {
-    if (err)
-      res.send(err);
-    res.json(profile);
-  });
+  Profile.findOneAnUpdate(
+    {_id: req.params.profileId},
+    req.body,
+    {new: true},
+    function(err, profile) {
+      if (err){
+        res.status(400).send(err);
+      }
+      res.json(profile);
+    }
+  );
 };
 
 exports.delete_a_profile = function(req, res) {
-  Profile.remove({
-    _id: req.params.profileId
-  }, function(err, profile) {
-    if (err)
-      res.send(err);
-    res.json({ message: 'Profile successfully deleted' });
-  });
+  Profile.remove(
+    {_id: req.params.profileId},
+    function(err, profile) {
+      if (err){
+        res.status(400).send(err);
+      }
+      res.json({ message: 'Profile successfully deleted' });
+    }
+  );
 };
