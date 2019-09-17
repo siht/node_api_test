89ea9367fd0ce0c663f6b8e0477beab0ed519038
carlos <radix_10339@hotmail.com>
2019-09-17 02:08:15 -0500

    remove unnecesary lines

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
-  Profile = require('./api/models/profileModel'), //created model loading here
-  bodyParser = require('body-parser');
+  Profile = require('./api/models/profileModel'); //created model loading here
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO || 'mongodb://localhost:27017/Profile'); 
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
@@ -19,6 +15,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
 // app.use(function(req, res) {
 //   res.status(404).send({url: req.originalUrl + ' not found'})
 // });
