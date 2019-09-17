6fcf3ca847f993d4149ca23da1b4c0019721353d
carlos <radix_10339@hotmail.com>
2019-09-16 21:03:39 -0500

    update routes
    because previous are not understandable

--- api/routes/profileRoutes.js
+++ api/routes/profileRoutes.js
@@ -4,11 +4,12 @@ module.exports = function(app) {
 
   // profiles Routes
   app.route('/profiles')
-    .get(profiles.list_all_profiles)
+    .get(profiles.list_all_profiles);
+
+  app.route('/profile/new')
     .post(profiles.create_a_profile);
 
-
-  app.route('/profiles/:profileId')
+  app.route('/profile/:profileId')
     .get(profiles.read_a_profile)
     .put(profiles.update_a_profile)
     .delete(profiles.delete_a_profile);
