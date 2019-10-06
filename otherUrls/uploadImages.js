f64d5226b420e584b548a65973766a26d24b08dc
carlos <radix_10339@hotmail.com>
2019-10-06 12:33:35 -0500

    minor fixes

--- otherUrls/uploadImages.js
+++ otherUrls/uploadImages.js
@@ -1,46 +1,46 @@
 'use strict';
-var mongoose = require('mongoose'),
-  Profile = mongoose.model('Profile');
-const ImgurStorage = require('multer-storage-imgur');
-const multer = require('multer');
-const upload = multer(
-    {
-        storage: ImgurStorage({ 'clientId': process.env.IMGUR_CLIENT_ID})
-    }
-).single('image');
+const mongoose = require('mongoose')
+  ,Profile = mongoose.model('Profile')
+  ,multer = require('multer')
+  ,ImgurStorage = require('multer-storage-imgur')
+  ,upload = multer(
+    {storage: ImgurStorage({ 'clientId': process.env.IMGUR_CLIENT_ID})}
+  );
 
 module.exports = function(app) {
-    app.post('/profile/:profileId/upload-image', (req, res) => {
-        upload(req, res, function (err) {
-            if (err instanceof multer.MulterError) {
-                console.error(err);
-                res.status(500).json(err);
-            } else if (err) {
-                console.error(err);
-                res.status(500).json(err);
+  app.post('/profile/:profileId/upload-image', (req, res) => {
+    upload.single('image')(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        console.error(err);
+        res.status(500).json(err);
+      } else if (err) {
+        console.error(err);
+        res.status(500).json(err);
+      }
+      else{
+        Profile.findOneAndUpdate(
+          {_id: req.params.profileId},
+          {imagen: req.file.data.link},
+          function(err, profile) {
+            if (err){
+              console.error(err);
+              res.status(400).send(err);
             }
             else{
-                Profile.findOneAndUpdate(
-                    {_id: req.params.profileId},
-                    {imagen: req.file.data.link},
-                    function(err, profile) {
-                      if (err){
-                        console.error(err);
-                        res.status(400).send(err);
-                      }
-                      Profile.findById(
-                        req.params.profileId,
-                        function(err, profile) {
-                          if (err){
-                            console.error(err);
-                            res.status(400).send(err);
-                          }
-                          res.json(profile);
-                        }
-                      );
-                    }
-                );
+              Profile.findById(
+                profile._id,
+                function(err, profile) {
+                  if (err){
+                    console.error(err);
+                    res.status(400).send(err);
+                  }
+                  res.json(profile);
+                }
+              );   
             }
-        })
-    });
+          }
+        );
+      }
+    })
+  });
 }
